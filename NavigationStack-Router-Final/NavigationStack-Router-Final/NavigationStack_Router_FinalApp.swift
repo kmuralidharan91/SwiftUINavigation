5824d5715579cbbc54a6b62bdac93de42c46b3d8
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2024-05-19 21:34:24 +0100

    Add files via upload

---
+++ NavigationStack-Router-Final/NavigationStack-Router-Final/NavigationStack_Router_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStack_Router_FinalApp.swift
+//  NavigationStack-Router-Final
+//
+//  Created by Muralidharan Kathiresan on 19/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStack_Router_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContinentsView()
+        }
+    }
+}
---
+++ NavigationStack-Router-Starter/NavigationStack-Router-Starter/Continent.swift
@@ -0,0 +1,33 @@
+//
+//  Continent.swift
+//  NavigationView
+//
+//  Created by Muralidharan Kathiresan on 18/05/24.
+//
+
+import Foundation
+
+// MARK: - Continent
+struct Continent: Codable, Identifiable, Hashable {
+    let continent: String
+    let countries: [Country]
+    
+    var id: UUID {
+        return UUID()
+    }
+    
+    var name: String {
+        return continent
+    }
+}
+
+// MARK: - Country
+struct Country: Codable, Identifiable, Hashable {
+    let name, emoji, population, language: String
+    
+    var id: UUID {
+        return UUID()
+    }
+}
+
+typealias Continents = [Continent]
