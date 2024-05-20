87898ee0d135c216447b29de8bcc23864ddd58eb
Muralidharan Kathiresan <kmuralidharan91@gmail.com>
2024-05-20 01:17:48 +0100

    State Restoration of Router

--- NavigationStack-Router-Final/NavigationStack-Router-Final/Router.swift
+++ NavigationStack-Router-Final/NavigationStack-Router-Final/Router.swift
@@ -5,11 +5,37 @@
 //  Created by Muralidharan Kathiresan on 19/05/24.
 //
 
-import Foundation
+import SwiftUI
 
 final class Router: ObservableObject {
-    @Published var routes = [Route]()
+    @Published var routes = [Route]() {
+        didSet {
+            save()
+        }
+    }
     
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPathStore")
+
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Route].self, from: data) {
+                routes = decoded
+                return
+            }
+        }
+    }
+
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(routes)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+
+extension Router {
     func push(to screen: Route) {
         guard !routes.contains(screen) else {
             return
